1e594f5533769ff64a42854cce023a3da39ddc05
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-06-04 16:00:08 +0200

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,22 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/HomePage/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <HomePage />
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/answer_clarification" element={null} />
+          <Route path="" element={null} />
+          <Route path="" element={null} />
+          <Route path="" element={null} />
+          <Route path="" element={null} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
