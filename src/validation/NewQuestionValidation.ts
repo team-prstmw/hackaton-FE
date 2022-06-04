ff747dc2c94eb8905418fa48b92f3ff0917c6f8c
Filip Hałoń <halon.filip@gmail.com>
2022-06-04 23:44:38 +0200

    Add Polish validation messages to the new question form

--- src/validation/NewQuestionValidation.ts
+++ src/validation/NewQuestionValidation.ts
@@ -1,12 +1,15 @@
 import * as Yup from "yup";
 import { CATEGORIES } from "../constants/forms";
+import { ONE_OF_CHOICES_MESSAGE, REQUIRED_MESSAGE } from "./messages";
 
 const NewQuestionValidation = Yup.object({
-  content: Yup.string().required(),
-  answer: Yup.boolean().required(),
-  explanation: Yup.string().required(),
-  category: Yup.string().oneOf(CATEGORIES).required(),
-  link: Yup.string().required(),
+  content: Yup.string().required(REQUIRED_MESSAGE),
+  answer: Yup.boolean().required(REQUIRED_MESSAGE),
+  explanation: Yup.string().required(REQUIRED_MESSAGE),
+  category: Yup.string()
+    .oneOf(CATEGORIES, ONE_OF_CHOICES_MESSAGE)
+    .required(REQUIRED_MESSAGE),
+  link: Yup.string().required(REQUIRED_MESSAGE),
 });
 
 export default NewQuestionValidation;
---
+++ src/validation/messages.ts
@@ -0,0 +1,2 @@
+export const REQUIRED_MESSAGE = "To pole jest wymagane";
+export const ONE_OF_CHOICES_MESSAGE = "Musi być jednym z listy";
