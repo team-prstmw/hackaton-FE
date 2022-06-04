010f9076e1efabe4ab9567108c41b0aa45d40717
Filip Hałoń <halon.filip@gmail.com>
2022-06-05 01:06:33 +0200

    Enable fetching highscores from the server

--- src/components/TopScoresTable/TopScoresTable.tsx
+++ src/components/TopScoresTable/TopScoresTable.tsx
@@ -1,8 +1,9 @@
 import { useMemo } from "react";
 import { useTable, usePagination } from "react-table";
 
-import { defaultData } from "./TopScoresTableDefaultData";
 import styles from "./TopScoresTable.module.scss";
+import { useApiGet } from "../../hooks/useApi";
+import { SCORES_ENDPOINT } from "../../urls/api";
 
 function Table({ columns, data }: any) {
   const { getTableProps, getTableBodyProps, headerGroups, page, prepareRow } =
@@ -57,7 +58,9 @@ export function TopScoresTable() {
     []
   );
 
-  const data = [...defaultData].sort((a, b) => (a.score < b.score ? 1 : -1));
+  const { data: payload } = useApiGet({ path: SCORES_ENDPOINT });
+
+  const data = [...payload].sort((a, b) => (a.score < b.score ? 1 : -1));
 
   return <Table columns={columns} data={data} />;
 }
