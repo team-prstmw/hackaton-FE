40762e7943c16360d7f9da28082b298a161fd022
MCaliJr <MartinK.MoC@gmail.com>
2022-06-04 22:50:28 +0200

    Restyle new Q page"

--- src/pages/NewQuestionPage/NewQuestionPage.tsx
+++ src/pages/NewQuestionPage/NewQuestionPage.tsx
@@ -11,6 +11,7 @@ import SubmitButton from "../../components/Forms/SubmitButton/SubmitButton";
 import { useApiSend } from "../../hooks/useApi";
 import { CREATE_QUESTION_ENDPOINT } from "../../urls/api";
 import { Navbar } from "../../components/Navbar/Navbar";
+import { Card } from "../../components/Card/Card";
 
 interface NewQuestionData {
   content: string;
@@ -33,32 +34,36 @@ const NewQuestionPage = () => {
     apiSend({ path: CREATE_QUESTION_ENDPOINT, data: data });
   }
 
+  const data = (
+    <FormProvider {...methods}>
+      <form
+        className={styles.form}
+        onSubmit={handleSubmit(onSubmit)}
+        noValidate
+      >
+        <div>
+          <Textarea id="content" label="Pytanie" />
+        </div>
+        <Label>Odpowiedzi</Label>
+        <div>
+          <Textarea id="explanation" label="Wyjaśnienie" />
+        </div>
+        <div>
+          <Select id="category" label="Kategoria" />
+        </div>
+        <div>
+          <Input id="link" label="Źródło" />
+        </div>
+        <SubmitButton />
+      </form>
+    </FormProvider>
+  );
+
   return (
     <>
       <Navbar />
       <div className={styles.formContainer}>
-        <FormProvider {...methods}>
-          <form
-            className={styles.form}
-            onSubmit={handleSubmit(onSubmit)}
-            noValidate
-          >
-            <div>
-              <Textarea id="content" label="Pytanie" />
-            </div>
-            <Label>Odpowiedzi</Label>
-            <div>
-              <Textarea id="explanation" label="Wyjaśnienie" />
-            </div>
-            <div>
-              <Select id="category" label="Kategoria" />
-            </div>
-            <div>
-              <Input id="link" label="Źródło" />
-            </div>
-            <SubmitButton />
-          </form>
-        </FormProvider>
+        <Card data={data} />
       </div>
     </>
   );
